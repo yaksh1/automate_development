d8177bc3b914f312f60e618a84f3ae0798f28b01
yaksh gandhi <95672067+yaksh1@users.noreply.github.com>
2025-03-19 03:13:36 +0530

    complete

--- MyService.java
+++ MyService.java
@@ -1,12 +1,8 @@
-/**
- * The MyService class represents a service with a name and description.
- * It serves as a basic blueprint for defining services with these attributes.
- */
 class MyService {
 
-  // The name of the service
+
   private String name;
 
-  // A brief description of the service
+
   private String description;
 }
